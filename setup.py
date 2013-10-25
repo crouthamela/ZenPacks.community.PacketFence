17d3ad63ecbca0380e9040ea208697a7de531a15
Andrew Crouthamel <crouthamela@gmail.com>
2013-10-25 08:32:27 -0400

    Add RRD minimum to count, fix erroneous )

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.PacketFence"
-VERSION = "1.0.0"
+VERSION = "1.0.2"
 AUTHOR = "Andrew Crouthamel"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
