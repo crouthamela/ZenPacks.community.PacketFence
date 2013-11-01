8e1fc2d71f4a2ec00d8785f8c15bea6c5f0b8b79
Andrew Crouthamel <crouthamela@gmail.com>
2013-11-01 16:35:25 -0400

    New version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.PacketFence"
-VERSION = "1.0.4"
+VERSION = "1.0.5"
 AUTHOR = "Andrew Crouthamel"
 LICENSE = "GPLv2"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
